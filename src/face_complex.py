1fac5e9a7f85e79b1337b1391638f00e3c232e38
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2021-11-20 17:42:32 +1100

    Implement connected components for face complex

--- src/face_complex.py
+++ src/face_complex.py
@@ -8,8 +8,8 @@ Location = tuple[int, int]
 class Complex(object):
     def __init__(self, faces: list[list[int]]):
         self._faces = faces
-        self._neighbors = faceNeighbors(faces)
-        self._degrees = vertexDegrees(faces)
+        self._neighbors = _faceNeighbors(faces)
+        self._degrees = _vertexDegrees(faces)
         self._vertices = [v for v, m in enumerate(self._degrees) if m > 0]
 
     @property
@@ -24,35 +24,62 @@ class Complex(object):
     def nrFaces(self) -> int:
         return len(self._faces)
 
-    @property
-    def faceVertices(self) -> Iterator[list[int]]:
-        return iter(self._faces)
-
     def degree(self, v: int) -> int:
         return self._degrees[v] if 0 <= v < len(self._degrees) else 0
+
+    def facesVertices(self, f: int) -> list[int]:
+        return self._faces[f] if 0 <= f < len(self._faces) else []
 
     def faceNeighbors(self, f: int) -> list[Optional[Location]]:
         return self._neighbors[f] if 0 <= f < len(self._neighbors) else []
 
 
 
-def faceNeighbors(faces: list[list[int]]) -> list[list[Optional[Location]]]:
+def components(complex: Complex) -> list[Complex]:
+    result: list[Complex] = []
+    seen: set[int] = set()
+
+    for f0 in range(complex.nrFaces):
+        if not f0 in seen:
+            seen.add(f0)
+            queue = [f0]
+            k = 0
+
+            while k < len(queue):
+                f = queue[k]
+                k += 1
+
+                for neighbor in complex.faceNeighbors(f):
+                    if neighbor is not None:
+                        g, _ = neighbor
+                        if not g in seen:
+                            seen.add(g)
+                            queue.append(g)
+
+            result.append(Complex([complex.facesVertices(f) for f in queue]))
+
+    return result
+
+
+# -- Helper functions
+
+def _faceNeighbors(faces: list[list[int]]) -> list[list[Optional[Location]]]:
     edgeLocation: dict[OrientedEdge, Location] = {}
 
     for i, face in enumerate(faces):
-        for k, (v, w) in enumerate(cyclicPairs(face)):
+        for k, (v, w) in enumerate(_cyclicPairs(face)):
             if edgeLocation.get((v, w)) is None:
                 edgeLocation[(v, w)] = (i, k)
             else:
                 raise ValueError('an oriented edge appears more than once')
 
     return [
-        [edgeLocation.get((w, v)) for v, w in cyclicPairs(face)]
+        [edgeLocation.get((w, v)) for v, w in _cyclicPairs(face)]
         for face in faces
     ]
 
 
-def vertexDegrees(faces: list[list[int]]) -> list[int]:
+def _vertexDegrees(faces: list[list[int]]) -> list[int]:
     maxVertex = max(max(f) for f in faces)
     degree = [0] * (maxVertex + 1)
 
@@ -63,7 +90,7 @@ def vertexDegrees(faces: list[list[int]]) -> list[int]:
     return degree
 
 
-def cyclicPairs(indices: list[T]) -> list[tuple[T, T]]:
+def _cyclicPairs(indices: list[T]) -> list[tuple[T, T]]:
     return list(zip(indices, indices[1:] + indices[:1]))
 
 
@@ -79,3 +106,5 @@ if __name__ == '__main__':
     complex = Complex(
         [ [ v for v in f['vertices'] ] for f in data['faces'] ]
     )
+
+    print("%d components" % len(components(complex)))
