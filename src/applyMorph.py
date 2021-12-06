c056bab74ab7d1c4e78bac7380233c390db7473c
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2021-12-06 12:21:38 +1100

    Add argument parsing to the applyMorph script

--- src/applyMorph.py
+++ src/applyMorph.py
@@ -1,3 +1,82 @@
+def run():
+    import os.path
+
+    import obj
+    from match import match
+
+    args = parseArguments()
+
+    topoBase, dataBase = loadAndProcessMesh(args.basepath)
+    if args.verbose:
+        print("Counts for base: %s" % symmetryCounts(topoBase))
+
+    topoMorph, dataMorph = loadAndProcessMesh(args.morphpath)
+    if args.verbose:
+        print("Counts for morph: %s" % symmetryCounts(topoMorph))
+
+    mapping = match(topoBase, topoMorph)
+    if args.verbose:
+        print("Mapping (of %d vertices):\n%s" % (len(mapping), mapping))
+
+    dataOut = dataBase.copy()
+    dataOut["vertices"] = dataOut["vertices"].copy()
+
+    vertsOut = dataOut["vertices"]
+    vertsMorph = dataMorph["vertices"]
+
+    for v, w in mapping:
+        vertsOut[v - 1] = vertsMorph[w - 1]
+
+    if args.show:
+        vertsIn = dataBase["vertices"]
+        display(
+            { "vertices": vertsIn - [0.1, 0.0, 0.0], "faces": dataBase["faces"] },
+            { "vertices": vertsOut, "faces": dataBase["faces"] }
+        )
+
+    outname, outext = os.path.splitext(args.outpath)
+    if outext != ".obj":
+        outname += outext
+
+    with open(outname + ".obj", "w") as fp:
+        obj.save(fp, dataOut, outname + ".mtl")
+
+
+def parseArguments():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Apply a morph to a mesh.')
+    parser.add_argument(
+        'basepath',
+        type=str,
+        help='original mesh in OBJ format'
+    )
+    parser.add_argument(
+        'morphpath',
+        type=str,
+        help='morphed mesh in OBJ format'
+    )
+    parser.add_argument(
+        'outpath',
+        type=str,
+        help='path for the OBJ-formatted output data'
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        default=False,
+        help='print some information about the meshes and matching'
+    )
+    parser.add_argument(
+        '-s', '--show',
+        action='store_true',
+        default=False,
+        help='display the input and output via PolyScope'
+    )
+
+    return parser.parse_args()
+
+
 def loadAndProcessMesh(path):
     import obj
     from match import topology
@@ -11,6 +90,13 @@ def loadAndProcessMesh(path):
     )
 
     return topo, data
+
+
+def symmetryCounts(topo):
+    return [
+        [ [ len(vs) for vs in inst ] for inst in val ]
+        for val in topo.values()
+    ]
 
 
 def display(*meshes):
@@ -31,49 +117,4 @@ def display(*meshes):
 
 
 if __name__ == '__main__':
-    import sys
-
-    import obj
-    from match import match
-
-    topoBase, dataBase = loadAndProcessMesh(sys.argv[1])
-
-    symcounts = [
-        [ [ len(vs) for vs in inst ] for inst in val ]
-        for val in topoBase.values()
-    ]
-    print("Counts for base: %s" % " ".join(map(str, symcounts)))
-    print()
-
-    if len(sys.argv) > 2:
-        topoMorph, dataMorph = loadAndProcessMesh(sys.argv[2])
-
-        symcounts = [
-            [ [ len(vs) for vs in inst ] for inst in val ]
-            for val in topoMorph.values()
-        ]
-        print("Counts for morph: %s" % " ".join(map(str, symcounts)))
-        print()
-
-    mapping = match(topoBase, topoMorph)
-    print("Mapping (of %d vertices):\n%s" % (len(mapping), mapping))
-
-    dataOut = dataBase.copy()
-    dataOut["vertices"] = dataOut["vertices"].copy()
-
-    vertsIn = dataBase["vertices"]
-    vertsOut = dataOut["vertices"]
-    vertsMorph = dataMorph["vertices"]
-
-    for v, w in mapping:
-        vertsOut[v - 1] = vertsMorph[w - 1]
-
-    '''
-    display(
-        { "vertices": vertsIn - [0.1, 0.0, 0.0], "faces": dataBase["faces"] },
-        { "vertices": vertsOut, "faces": dataBase["faces"] }
-    )
-    #'''
-
-    with open("test.obj", "w") as fp:
-        obj.save(fp, dataOut, "test.mtl")
+    run()
