07820186759c17438a2ec0d7e9dc9eb266fb50d0
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2021-12-13 18:51:46 +1100

    Add a script for finding non-manifold meshes

---
+++ src/findNonManifolds.py
@@ -0,0 +1,24 @@
+import sys
+import obj
+
+
+def _cyclicPairs(indices):
+    return zip(indices, indices[1:] + indices[:1])
+
+
+for path in sys.argv[1:]:
+    try:
+        with open(path) as fp:
+            data = obj.load(fp, path)
+            faces = [ f["vertices"] for f in data["faces"] ]
+            edgeLocations = {}
+
+            for i, face in enumerate(faces):
+                for k, (v, w) in enumerate(_cyclicPairs(face)):
+                    edgeLocations.setdefault((v, w), []).append((i, k, False))
+                    edgeLocations.setdefault((w, v), []).append((i, k, True))
+
+            if any(len(a) > 2 for a in edgeLocations.values()):
+                print(path)
+    except:
+        pass
