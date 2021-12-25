43fa45cd764ede40892c148794840a65e7adb234
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2021-12-25 12:02:58 +1100

    In applyMorph, say if mapping is the identity

--- src/applyMorph.py
+++ src/applyMorph.py
@@ -19,7 +19,10 @@ def run():
         topoBase, topoMorph, minimumWeightAssignment, verbose=args.verbose
     )
     if args.verbose:
-        print("Mapping (of %d vertices):\n%s" % (len(mapping), mapping))
+        if all(u == v for u, v in mapping):
+            print("Mapping is identity on %d vertices" % len(mapping))
+        else:
+            print("Mapping (of %d vertices):\n%s" % (len(mapping), mapping))
 
     vertsOut = dataBase.vertices.copy()
     vertsMorph = dataMorph.vertices
