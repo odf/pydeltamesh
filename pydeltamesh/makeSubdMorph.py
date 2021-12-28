7f73360ac8334192f76df80706898bb5370ac7b4
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2021-12-28 17:50:53 +1100

    Complete deltas for subd morph script

--- pydeltamesh/makeSubdMorph.py
+++ pydeltamesh/makeSubdMorph.py
@@ -2,7 +2,7 @@ def run():
     from uuid import uuid4
 
     from .io.pmd import Deltas, MorphTarget, write_pmd
- 
+
     args = parseArguments()
 
     base = loadMesh(args.basepath, args.verbose)
@@ -78,30 +78,75 @@ def extractDeltas(base, morph, verbose=False):
     if verbose:
         print("Computing deltas...")
 
-    def norm(v):
-        return np.sqrt(np.dot(v, v))
-
-    verticesBase = base.vertices
-    normalsBase = base.normals
-    verticesMorph = morph.vertices
+    vertexLocations = {}
+    for i, f in enumerate(base.faces):
+        for j, v in enumerate(f.vertices):
+            vertexLocations.setdefault(v, []).append((i, j))
 
     deltaIndices = []
     deltaVectors = []
 
-    for i in range(len(verticesBase)):
-        d = verticesMorph[i] - verticesBase[i]
+    for i in range(len(base.vertices)):
+        d = morph.vertices[i] - base.vertices[i]
 
         if norm(d) > 1e-5:
-            u = normalsBase[i]
-            u /= norm(u)
+            n, u, v = deltaDirectionsAtVertex(i, base, vertexLocations)
 
             deltaIndices.append(i)
-            deltaVectors.append([np.dot(d, u), 0.0, 0.0])
+            deltaVectors.append([np.dot(d, n), np.dot(d, u), np.dot(d, v)])
 
     if verbose:
         print("Computed %d deltas." % len(deltaIndices))
 
-    return Deltas(len(verticesBase), deltaIndices, deltaVectors)
+    return Deltas(len(base.vertices), deltaIndices, deltaVectors)
+
+
+def deltaDirectionsAtVertex(v, base, vertexLocations):
+    import numpy as np
+
+    uDir = np.zeros(3)
+    vDir = np.zeros(3)
+
+    for i, j in vertexLocations[v]:
+        vs = base.faces[i].vertices
+        a = base.vertices[vs[j]]
+        b = base.vertices[vs[(j + 1) % len(vs)]]
+        c = base.vertices[vs[j - 1]]
+
+        tvs = base.faces[i].texverts
+        p = base.texverts[tvs[j]]
+        q = base.texverts[tvs[(j + 1) % len(tvs)]]
+        r = base.texverts[tvs[j - 1]]
+
+        if abs(cross2d(p, q) + cross2d(q, r) + cross2d(r, p)) > 1e-12:
+            for k, dir in [(0, vDir), (1, uDir)]:
+                if abs(q[k] - r[k]) > 1e-8:
+                    t = (p[k] - q[k]) / (r[k] - q[k])
+
+                    if 0 <= t <= 1:
+                        d = (1 - t) * b + t * c - a
+                        if (1 - t) * q[k - 1] + t * r[k - 1] < p[k - 1]:
+                            d = -d
+                        dir[:] += normalized(d)
+
+    n = normalized(base.normals[v])
+    u = normalized(uDir - np.dot(n, uDir) * n)
+    v = -normalized(vDir - np.dot(n, vDir) * n - np.dot(u, vDir) * u)
+
+    return n, u, v
+
+
+def norm(v):
+    return sum(x * x for x in v)**0.5
+
+
+def normalized(v):
+    n = norm(v)
+    return v / n if n > 0 else v
+
+
+def cross2d(v, w):
+    return v[0] * w[1] - v[1] * w[0]
 
 
 def writeInjectionPoseFile(actor, name, uuid, numbDeltas, verbose=False):
