7b284edca67a1dd914a2d55b82721e655249a6ed
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2024-10-23 19:14:06 +1100

    do some more work on the weaver script

--- texture_scripts/weaver.py
+++ texture_scripts/weaver.py
@@ -1,13 +1,17 @@
 from pydeltamesh.makeTexture import *
 
 
+def rem(a, b):
+    return (a % b + b) % b
+
+
 def maprange(x, from_min, from_max, to_min, to_max):
     t = (to_max - to_min) / (from_max - from_min)
     return (x - from_min) * t + to_min
 
 
 def threads(x, scale, gap):
-    a = maprange(x * scale % 1, gap, 1, 0, 1)
+    a = maprange(x * scale % 1, gap / 2, 1 - gap / 2, 0, 1)
     return (1 - 2 * abs(a - 0.5)).bias(0.9)
 
 
@@ -15,6 +19,9 @@ u = Input(U(), "u")
 v = Input(V(), "v")
 scale = Input(10, "scale")
 gap = Input(0.2, "gap")
+up_count = Input(3, "up_count")
+down_count = Input(2, "down_count")
+shift = Input(2, "shift")
 
 warp = threads(u, scale, gap)
 warp.name = "warp"
@@ -22,8 +29,18 @@ warp.name = "warp"
 weft = threads(v, scale, gap)
 weft.name = "weft"
 
+i_warp = (u * scale).floor()
+i_weft = (v * scale).floor()
+period = up_count + down_count
+
+weft_is_up = rem(i_warp - shift * i_weft, period) < up_count
+weft_is_up.name = "weft_is_up"
+
+weave = weft * weft_is_up + warp * (1 - weft_is_up)
+weave.name = "weave"
+
 with open("weaver_mktx.mt5", "w") as fp:
-    write_poser_file(fp, "weaver", [warp, weft])
+    write_poser_file(fp, "weaver", [warp, weft, weft_is_up, weave])
 
 from PIL import Image
-Image.fromarray(weft.data * 256).show()
+Image.fromarray(weave.data * 256).show()
