d34825919db313662129bccca7e4ec4eb308415b
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2024-11-01 12:08:54 +1100

    implement varying thread thicknesses

--- texture_scripts/weaver.py
+++ texture_scripts/weaver.py
@@ -5,31 +5,39 @@ def rem(a, b):
     return (a % b + b) % b
 
 
+def random(x):
+    t = x.sin() * 99371
+    return t - t.floor()
+
+
 def maprange(x, from_min, from_max, to_min, to_max):
     t = (to_max - to_min) / (from_max - from_min)
     return (x - from_min) * t + to_min
 
 
-def threads(x, scale, gap):
+def threads(x, scale, gap, gap_variation):
+    thread_nr = (x * scale).floor()
+    gap = gap + gap_variation * (random(thread_nr) - 0.5)
     a = maprange(x * scale % 1, gap / 2, 1 - gap / 2, 0, 1)
-    return (a * (1 - a)).sqrt()
+    return (a * (1 - a)).sqrt() * (1 - gap)
 
 
 u = Input(U(), "u")
 v = Input(V(), "v")
-scale = Input(10, "scale")
+scale = Input(20, "scale")
 gap = Input(0.4, "gap")
-up_count = Input(3, "up_count")
-down_count = Input(2, "down_count")
-shift = Input(2, "shift")
+gap_variation = Input(0.2, "gap_variation")
+up_count = Input(1, "up_count")
+down_count = Input(1, "down_count")
+shift = Input(1, "shift")
 
-warp = threads(u, scale, gap)
-weft = threads(v, scale, gap)
+warp = threads(u, scale, gap, gap_variation)
+weft = threads(v + 1, scale, gap, gap_variation)
 
 warp_mask_raw = warp > 0
 weft_mask_raw = weft > 0
 
-opacity = warp_mask_raw.max(weft_mask_raw)
+opacity = warp_mask_raw | weft_mask_raw
 opacity.name = "opacity"
 
 idx_warp = (u * scale).floor()
@@ -92,4 +100,4 @@ with open("weaver_mktx.mt5", "w") as fp:
     )
 
 from PIL import Image
-Image.fromarray(thread_v.data * 256).show()
+Image.fromarray(bump.data * 256).show()
